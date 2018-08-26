0201d500d205fd1c2cd6745aae82e39c36335438
Rishy Ahluwalia <rishy.ahluwalia26@gmail.com>
2018-08-27 00:20:37 +1000

    Added functionality to add and remove mock data

--- client/src/containers/ViewItemsPage.js
+++ client/src/containers/ViewItemsPage.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Container, Row, Col } from 'reactstrap';
+import { Button, Col } from 'reactstrap';
 import SideBar from '../components/SideBar';
 import Item from '../components/Item';
 import { getItems } from '../actions/itemActions';
-import { withRouter } from 'react-router-dom';
+import { FaPlus } from 'react-icons/fa';
+import { FaTrashAlt } from 'react-icons/fa';
+import axios from 'axios';
 
 class ViewItemsPage extends Component {
     constructor(props) {
-      super(props)
-    
-      if (!this.props.fetched) {
-        this.props.dispatch(getItems());
+        super(props)
+
+        if (!this.props.fetched) {
+            this.props.dispatch(getItems());
         }
     }
 
@@ -25,10 +27,30 @@ class ViewItemsPage extends Component {
         this.props.dispatch({ type: "CLEAR_ITEMS" })
     }
 
+    generateItems = (amount) => {
+        axios.get('/api/items/generateData/'+amount)
+        .then(this.props.dispatch(getItems()))
+        .catch(err=>console.log(err))
+
+    }
+
+    removeItems = () => {
+        axios.get('/api/items/deleteData/')
+        .then(this.props.dispatch(getItems()))
+        .catch(err=>console.log(err))
+
+    }
+
     render() {
         return (
-            <Col className="viewitems contentBg pl-4 pr-4 scroll" xs={!this.props.mobi?"10":"11"}>
-                <h3>Items For Purchase</h3>
+            <Col className="viewitems contentBg pl-4 pr-4 scroll" xs={!this.props.mobi ? "10" : "11"}>
+                <div className="d-flex">
+                    <h3 className="mr-2">Items For Purchase</h3>
+                    <div className="ml-auto">
+                        <Button size="sm" color="primary" className="mr-2" onClick={()=>this.generateItems(prompt("Enter an amount:"))}><FaPlus /></Button>
+                        <Button size="sm" color="danger" onClick={()=>this.removeItems()}><FaTrashAlt /></Button>
+                    </div>
+                </div>
                 <hr />
                 <div className="flexwrap justify-content-center">
                     {this.props.items.map(item => <Item key={item._id} item={item} mobi={this.props.mobi} />)}
@@ -40,10 +62,10 @@ class ViewItemsPage extends Component {
 
 
 const mapStateToProps = (state) => ({
-        authenticated: state.user.authenticated,
-        items: state.items.items,
-        fetched: state.items.fetched,
-        mobi: state.mobi.mobi
+    authenticated: state.user.authenticated,
+    items: state.items.items,
+    fetched: state.items.fetched,
+    mobi: state.mobi.mobi
 })
 
 
