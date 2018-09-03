1774f8b5be478c8eff5d072084f3df204553c51c
Luke Rafferty <lukeraffertydev@gmail.com>
2018-09-03 15:32:34 +1000

    initial view items in cart front end

--- client/src/containers/CartPage.js
+++ client/src/containers/CartPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Col } from 'reactstrap';
 import { notify } from '../actions/notificationActions';
+import Item from '../components/Item';
 
 
 class CartPage extends Component {
@@ -18,6 +19,9 @@ class CartPage extends Component {
             <Col className="contentBg pl-4 pr-4 pt-3 align-items-center" >
                 <h3>My Cart</h3>
                 <hr />
+                <div className="flexwrap justify-content-center">
+                    {this.props.authenticated && this.props.user.cart.map(item => <Item key={item._id} item={item} mobi={this.props.mobi} />)}
+                </div>
                 
             </Col>
         );
