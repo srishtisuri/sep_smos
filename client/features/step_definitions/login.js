53067c281654abf7f2d0f48c5f496d4bfdfefa4b
Christian <ctdamtoft@gmail.com>
2018-09-24 15:44:02 +1000

    added sample cucumber test

--- client/features/step_definitions/login.js
+++ client/features/step_definitions/login.js
@@ -5,12 +5,24 @@ import assert from 'assert';
 Given('a username of {string} and a password of {string}', function(username, password) {
   this.username = username;
   this.password = password;
+  this.accepted = false;
 })
 
 When('I click login', function() {
-  this.incrementBy(number)
+  if (this.username == 'valid' && this.password == 'test') {
+    this.accepted = true;
+  }
 })
 
-Then('the variable should contain {int}', function(number) {
-  assert.equal(this.variable, number)
+Then('the landing page should appear', function() {
+  assertLandingPage(this, true);
 })
+
+Then('the landing page should not appear', function() {
+  assertLandingPage(this, false);
+})
+
+
+function assertLandingPage(that, shouldAppear) {
+  assert.equal(that.accepted, shouldAppear);
+}
